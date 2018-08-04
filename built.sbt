a2c92d4c2b0850e05da41cd4932a2e8720e15953
alaindemour <alaindemour@gmail.com>
2018-08-04 15:41:05 -0400

    build with elasticsearch jar

--- built.sbt
+++ built.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1"
+// libraryDependencies += "org.elasticsearch.spark" %% "elasticsearch-spark-20" % "6.3.2"
+
--- src/main/scala/regsort.scala
+++ src/main/scala/regsort.scala
@@ -1,6 +1,9 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext    
+import org.apache.spark.SparkContext._
+import org.elasticsearch.spark._ 
 
-object RegSort {
+object RegSort {  
   def main(args: Array[String]) {
     val logFile = "./test.csv" // Should be some file on your system
     val spark = SparkSession.builder.appName("regsort").getOrCreate()
