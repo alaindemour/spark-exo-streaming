adc57d804fdeb91344e1fae818b360ea918cee2b
alaindemour <alaindemour@gmail.com>
2018-08-05 19:39:34 -0400

    was sucessful in running a filter

--- src/main/scala/regsort.scala
+++ src/main/scala/regsort.scala
@@ -1,7 +1,16 @@
 import org.apache.spark.sql.SparkSession
 import org.elasticsearch.spark._ 
+// import spark.implicits._
+
+
+
 
 object RegSort {  
+
+    val onlyCreations = (doc : (String, scala.collection.Map[String,AnyRef])) => { 
+      doc._2.contains("real")
+    }  
+
     def main(args: Array[String]) {
     // val logFile = "./test.csv" // Should be some file on your system
         
@@ -9,19 +18,23 @@ object RegSort {
       .builder
       .appName("regsort")
       .config("es.index.auto.create", "true")
+      .config("spark.network.timeout", "600s")
+      .config("spark.executor.heartbeatInterval", "120s")
+      .config("spark.executor.memory", "2g")
+      .config("spark.driver.memory", "8g")
+      .config("spark.driver.maxResultSize", "4g")
       .getOrCreate()
 
-    val artistAndProducts = spark.sparkContext.esRDD("artists_and_products/_doc")
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val artistsAndProducts = spark.sparkContext.esRDD("artists_and_products/_doc")
 
     // val logData = spark.read.textFile(logFile).cache()
     // val numAs = logData.filter(line => line.contains("a")).count()
     // val numBs = logData.filter(line => line.contains("b")).count()
-
-    val numAs = artistAndProducts.count()
     
-    val numBs = 7777
+    artistsAndProducts.filter(onlyCreations).collect()
 
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
     spark.stop()
   }
 }
